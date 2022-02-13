4cb99720a8facc648cd74d63ac7edae927295700
MatthewT12 <matttyndell@gmail.com>
2022-02-13 14:37:22 -0500

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("what is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // you can also log multiple values at once like this: console.log(playerName, playerHealth, playerAttack);
 
@@ -12,7 +13,11 @@ var fight = function() {
     // alert players that they are starting the round
     window.alert("Welcone to Robot Gladiators!")
     
-    // subtract the value of playerAttack from the value of enemyHealth and use that result to update the value in the enemyHealth variable
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'Fight' or 'SKIP' to choose.")
+
+    // if player chooses to fight, then fight
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+        // subtract the value of playerAttack from the value of enemyHealth and use that result to update the value in the enemyHealth variable
     enemyHealth = enemyHealth - playerAttack;
     
     // log a resulting message to the console so we know that it worked
@@ -43,6 +48,26 @@ var fight = function() {
     else {
         window.alert(playerName + " still has " + playerHealth + " health remaning.");
     }
-};
+    }
+        
+    else if (promptFight === "skip" || promptFight === "SKIP") {
+        // confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you want to quit?");
 
+        // if yes (true), leave the fight
+        if (confirmSkip) {
+            window.alert(playerName + " has chosen to skip this fight. Goodbye!");
+            // subtract money from playerMoney for skipping
+            playerMoney = playerMoney - 2;
+        }
+        // if no (false), ask the question again by running fight() again
+        else {
+            fight();
+        }
+        
+    }
+    else {
+        window.alert("You need to choose a valid option. Try again!");
+    }
+};
 fight();
